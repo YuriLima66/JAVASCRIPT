cd95a1010c679a30930856d93e6fde66145446ea
Yuri Lima <145035100+YuriLima66@users.noreply.github.com>
2024-12-03 15:15:12 -0300

    exclui pasta que estava sem uso

--- curso em video/exercicios/ex0015/script.js
+++ curso em video/exercicios/ex0015/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function verificar(){
+    var data= new Date()
+    var ano = data.getFullYear()
+    var form_ano= document.querySelector('input#txt_ano')
+    var res= document.getElementById('res')
+
+
+    if(form_ano.value.length == 0){
+        window.alert('Por favor, preencha o campo com o ano!')
+    }else{
+        var form_sex = document.getElementsByName('rad_sex')
+        var idade= ano- Number(form_ano.value)
+        res.innerHTML= `Idade calculada :`
+        
+    }
+}
